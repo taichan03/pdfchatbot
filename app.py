95b72666d370f416a794fe9ab4b97e007bfcf3f0
sixer <tai.c.chan3@gmail.com>
2023-05-06 15:41:53 -0400

    add pdf reader

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from dotenv import load_dotenv
 import os
 import streamlit as st
+from PyPDF2 import PdfReader
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask your PDF")
 
-    pdf = st.file_uploader("Upload your PDF")
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
 
-if __name__== '__main__':
+
+if __name__ == "__main__":
     main()

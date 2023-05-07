07d416c4c889a384c02185dec583ed80262d56f3
sixer <tai.c.chan3@gmail.com>
2023-05-06 22:42:19 -0400

    final

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
 
 
 def main():
@@ -36,6 +38,11 @@ def main():
         if user_question:
             docs = knowledge_base.similarity_search(user_question)
             # st.write(docs)
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+            response = chain.run(input_documents=docs, question=user_question)
+
+            st.write(response)
 
 
 if __name__ == "__main__":
